914d46d13ef5e27d9f002646cb3ffa642b7f5115
Young Suk Ahn Park <ys.ahnpark@hanaloop.com>
2025-01-14 11:46:37 +0900

    Fix connectivity with ML. Fix services-down script missing project name

--- scripts/services-down.sh
+++ scripts/services-down.sh
@@ -1,3 +1,3 @@
-docker-compose --env-file .env.docker-compose-onprem -f docker-compose-withenvoy.yml down
+docker compose -p haneco_onprem  --env-file .env.docker-compose-onprem -f docker-compose-withenvoy.yml down
 
 # env $(cat ".env.docker-compose-onprem.wocomment") docker-compose  -f docker-compose-withenvoy.yml down
--- scripts/services-up.sh
+++ scripts/services-up.sh
@@ -1,1 +1,6 @@
-OPENAI_API_KEY= CIPHER_KEY= DB_PASSWORD= docker-compose -p haneco_onprem  --env-file .env.docker-compose-onprem -f docker-compose-withenvoy.yml up
+
+if [[ "$1" == "-d" ]]; then
+  echo "Starting hanaeco services in detached mode..."
+fi
+
+OPENAI_API_KEY= CIPHER_KEY= DB_PASSWORD= docker compose -p haneco_onprem  --env-file .env.docker-compose-onprem -f docker-compose-withenvoy.yml up ${1:-}
